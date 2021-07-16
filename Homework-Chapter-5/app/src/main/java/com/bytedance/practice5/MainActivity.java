c4748ed5e43de48b9b45bee5090b552acbc77838
ChenXuzheng <root@raynor-tp.localdomain>
2021-07-16 22:35:19 +0800

    完成hw5

---
+++ Homework-Chapter-5/app/src/main/java/com/bytedance/practice5/MainActivity.java
@@ -0,0 +1,137 @@
+package com.bytedance.practice5;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.bytedance.practice5.model.MessageListResponse;
+import com.bytedance.practice5.socket.SocketActivity;
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import com.bytedance.practice5.model.Message;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "chapter5";
+    private FeedAdapter adapter = new FeedAdapter();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Fresco.initialize(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.rv_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        findViewById(R.id.btn_upload).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,UploadActivity.class);
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.btn_mine).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData(Constants.STUDENT_ID);
+            }
+        });
+
+        findViewById(R.id.btn_all).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData(null);
+            }
+        });
+        findViewById(R.id.btn_socket).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SocketActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+    // 2
+    // 用HttpUrlConnection实现获取留言列表数据，用Gson解析数据，更新UI（调用adapter.setData()方法）
+    // 注意网络请求和UI更新分别应该放在哪个线程中
+    private void getData(String studentId){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List<Message>res=getMessagesFromRemote(studentId);
+                if(res!=null){
+                    new Handler(getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.setData(res);
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    private List<Message> getMessagesFromRemote(String studentId){
+        String urlStr;
+        if(studentId!=null){
+           urlStr =String.format("%smessages?student_id=%s",Constants.BASE_URL,studentId);
+        } else{
+            urlStr =String.format("%smessages",Constants.BASE_URL);
+        }
+        Log.d(TAG,"url is "+urlStr);
+        MessageListResponse result=null;
+        try {
+            URL url=new URL(urlStr);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(5000);
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("token",Constants.token);  //设置header中的token
+
+            if (conn.getResponseCode() == 200){
+                InputStream in = conn.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                result = new Gson().fromJson(reader, new TypeToken<MessageListResponse>() {
+                }.getType());
+                reader.close();
+                in.close();
+
+            } else{
+                throw new  IOException("没找到欸QAQ");
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MainActivity.this,"网络异常"+e.toString(),Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        if(result!=null&&result.feeds!=null){
+            return result.feeds;  //拿到了值
+        }
+        return  null;
+    }
+
+}
---
+++ Homework-Chapter-5/readme.md
@@ -0,0 +1,37 @@
+## Homework5
+
+
+
+- 完成了所有TODO
+
+- ==NOTE== 当我们访问http://www.zju.edu.cn的时候会被重定向到https上，所以会有301出现，重定向到了https://www.zju.edu.cn, 尽管我尝试了443端口，然是仍然无法一次就拿到200
+
+- TODO7说明：
+
+  刚开始的时候我是直接按照Postman提供的HTTP的代码，很方便， 但是 但是！
+
+  有 大问题
+
+  ![image-20210716223158815](https://pic.raynor.top/images/2021/07/16/image-20210716223158815.png)
+
+  可以看到我画红线的两个地方是不正确的，我参考了网上的资料才最终发现。。。。
+
+  
+
+  
+
+运行的截图如下所示
+
+- 我的留言
+
+  ![image-20210716223318836](https://pic.raynor.top/images/2021/07/16/image-20210716223318836.png)
+
+- 留言板
+
+  ![image-20210716223400037](https://pic.raynor.top/images/2021/07/16/image-20210716223400037.png)
+
+- 上传无法展示，这里不再说明
+
+- socket
+
+  ![image-20210716223427146](https://pic.raynor.top/images/2021/07/16/image-20210716223427146.png)
